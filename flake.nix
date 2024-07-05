0d48ea7cd48b7f7a774ca03488ea7af5c1e15813
Mutable Figment <147920149+mutablefigment@users.noreply.github.com>
2024-07-05 19:10:44 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = with pkgs; [ 
+                    edk2
+                    edk2-uefi-shell
+                    OVMF
+                    qemu
+                  ];
+
+                  enterShell = ''
+                    echo "${pkgs.OVMF.fd}/FV/OVMF.fd"
+                  '';
+
+                  languages.zig.enable = true;
+                  languages.c.enable = true;
+
+                  processes.emu.exec = "${pkgs.qemu}/bin/qemu-system-x86_64 -bios ${pkgs.OVMF.fd}/FV/OVMF.fd -hdd fat:rw:./drive";
+                }
+              ];
+            };
+          });
+    };
+}
---
+++ src/main.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+const uefi = std.os.uefi;
+
+pub fn main() void {
+    const con_out = uefi.system_table.con_out.?;
+    const boot_services = uefi.system_table.boot_services.?;
+
+    // Clear the screen
+    _ = con_out.reset(false);
+
+    // Print some text
+    //_ = con_out.outputString("test\r\n");
+
+    // Wait 5 seconds
+    _ = boot_services.stall(5 * 1000 * 1000);
+}
